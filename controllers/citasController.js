06fd16accadca036cbbf230f8f3ee6a77271191e
ragarsa <rarosagar.igf@gmail.com>
2021-04-15 01:09:52 -0500

    Testin sql database with controller

--- controllers/citasController.js
+++ controllers/citasController.js
@@ -1,4 +1,3 @@
-const { connect } = require('../database');
 const Db = require('../database');
 const connectDb = Db.connect
 
@@ -6,17 +5,30 @@ const connectDb = Db.connect
 
 const citas = async (req, res) => {
     try {
-    
-        res.send({message:'Volvío a salir bien'})
-        console.log(req.body);
-        console.log('-------');
         
+        
+        const bodyMessage = await req.body;
+        console.log(bodyMessage);
+        
+        const query = `INSERT INTO reservaciones(nombre, apellido, hora, fecha, servicios) 
+        VALUES ('${bodyMessage.name}','${bodyMessage.apellido}','${bodyMessage.hora}','${bodyMessage.fecha}','${bodyMessage.servicios}');`;
+        
+        connectDb.query(query, (err, result) => {
+            if (err) throw err;
+            console.log(result)
+        });
+
+        res.send({message:'Volvío  bien'});
+    
         
         
     }catch(e) {
         res.send({message:'Algo salió mal'})
+        console.log(e);
     }
     
+
+
 };
 
 module.exports = {
